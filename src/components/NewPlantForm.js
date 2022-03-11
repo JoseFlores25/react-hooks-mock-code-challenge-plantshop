f8d83b55e042ae438d3b399d821510fa584356ef
Jose <flores.jose2185@gmail.com>
2022-03-10 19:23:27 -0500

    Done

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,45 @@
-import React from "react";
+function NewPlantForm({ onAddPlant, newPlant, setNewPlant }) {
+  // const [newPlant, setNewPlant] = useState({
+  //   name: '',
+  //   price: '',
+  //   image: ''
+  // })
 
-function NewPlantForm() {
+  function handleChange(e) {
+    setNewPlant({ ...newPlant, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onAddPlant(newPlant);
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Plant name"
+          value={newPlant.name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image URL"
+          value={newPlant.image}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="price"
+          step="0.01"
+          placeholder="Price"
+          value={newPlant.price}
+          onChange={handleChange}
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,16 +1,105 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+  const [newPlant, setNewPlant] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  let plantsToDisplay = [...plants];
+  plantsToDisplay = plantsToDisplay.filter(
+    (plant) =>
+      plant.name.includes(search) ||
+      plant.name.toLowerCase().includes(search) ||
+      plant.name.toUpperCase().includes(search)
+  );
+
+  useEffect(() => {
+    fetch("http://localhost:3003/plants")
+      .then((r) => r.json())
+      .then((data) => {
+        setPlants((currentPlants) => data);
+      });
+  }, []);
+
+  function onAddPlant(newPlant) {
+    console.log(newPlant);
+
+    const indexOfPlant = plants.findIndex(
+      (plantInArray) => plantInArray.id === newPlant.id
+    );
+    console.log(indexOfPlant);
+    if (indexOfPlant !== -1) {
+      console.log("plant already exists");
+      const copyOfPlants = [...plants];
+      copyOfPlants.splice(indexOfPlant, 1, newPlant);
+      console.log(copyOfPlants);
+
+      fetch(`http://localhost:3003/plants/${newPlant.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPlant),
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          setPlants((currentPlants) => copyOfPlants);
+          console.log(data);
+        });
+    } else {
+      fetch("http://localhost:3003/plants", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPlant),
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          setPlants((currentPlant) => [
+            ...plants,
+            {
+              ...data,
+              id: data.id,
+            },
+          ]);
+        });
+    }
+  }
+
+  function handleDelete(id) {
+    fetch(`http://localhost:3003/plants/${id}`, {
+      method: "DELETE",
+    });
+    setPlants(plants.filter((plant) => plant.id !== id));
+  }
+
+  function handlePriceEdit(plant) {
+    console.log(plant);
+    setNewPlant(plant);
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm
+        onAddPlant={onAddPlant}
+        newPlant={newPlant}
+        setNewPlant={setNewPlant}
+      />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList
+        plants={plantsToDisplay}
+        handleDelete={handleDelete}
+        handlePriceEdit={handlePriceEdit}
+      />
     </main>
   );
 }
-
 export default PlantPage;
